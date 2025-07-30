c8f7d8239261be60a66966048e612a312387a8a2
Priyaranjan <cse.priyaranjan@gmail.com>
2025-07-30 17:40:32 +0530

    Update userProfile.jsx

--- frontend/src/components/userProfile.jsx
+++ frontend/src/components/userProfile.jsx
@@ -38,7 +38,7 @@ const UserProfile = () => {
   }, []);
 
   return (
-    <div className="max-w-4xl mx-auto mt-10 p-6 bg-white rounded-lg shadow">
+    <div className="max-w-4xl mx-auto mt-10 mb-10 p-6 bg-white rounded-lg shadow">
       <button
         onClick={handleLogout}
         className="px-4 py-2  bg-red-500 text-white rounded hover:bg-red-600 float-right"
